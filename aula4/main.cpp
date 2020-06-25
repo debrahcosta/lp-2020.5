580a528ceada02a8fccfc618fcafd1e94c0bcaa9
déborah <deborahcosta.f@hotmail.com>
2020-06-25 14:46:40 -0300

    código da atividade

--- aula4/main.cpp
+++ aula4/main.cpp
@@ -1,29 +1,60 @@
-#include <fstream>
 #include <iostream>
 #include <string>
+#include <fstream>
+/*presença da aula*/
+
+void show_usage(const std::string &prog_name);
 
 int main(int argc, char *argv[]){
-    std::ifstream file("messages2.txt");
 
-    if (!file.is_open()){
-        std::cerr <<"Arquivo não existe ou sem permissão de leitura"<<std::endl;
+    if(argc==1){
+        show_usage(argv[0]);
+        return 1;
+    }
+
+    std::string action = argv[1];
+
+    if(action!="add" && action!="list"){
+        show_usage(argv[0]);
         return 1;
     }
 
     std::string message;
-    int line_number =0;
 
-    while (!file.eof()){
-        ++line_number;
-        std::getline(file,message);
-        std::cout<< "tamanho da mensagem: "<< message.size()<<std::endl;
-        if (message.size()==0){
-            continue;
+    if(argc==2 && action=="add") {
+        std::cout << "Informe a mensagem: " << std::endl;
+        std::getline(std::cin, message);
+    }else if (argc==2 && action=="list"){
+        std::ifstream file("arquivo.txt");
+
+        if (!file.is_open()){
+            std::cerr <<"Arquivo não existe ou sem permissão de leitura"<<std::endl;
+            return 1;
         }
-        std::cout<< line_number<<". "<<message <<std::endl;
+
+        while (!file.eof()){
+            std::getline(file,message);
+            std::cout<< message<<std::endl;
+        }
+
+        file.close();
+        return 0;
+
+    }else{
+        message = argv[2];
     }
 
-    file.close();
+    std::ofstream arquivo_saida("arquivo.txt",std::ios::app);
+    arquivo_saida << message<<std::endl;
+    if(! arquivo_saida.is_open()){
+        std::cerr<<"O arquivo não pode ser criado" <<std::endl;
+        return 1;
+    }
+    arquivo_saida.close(); /*só preciso do close pra fechar em momento específico*/
+    std::cout<<"Mensagem Adicionada"<<std::endl;
 
     return 0;
+}
+void show_usage(const std::string &prog_name) {
+    std::cout << "Uso: " << prog_name << " add <mensagem> ou "<<prog_name<<" list" << std::endl;
 }

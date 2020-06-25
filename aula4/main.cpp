bcb6f3d492a693e8b78845299658bea5202406b6
déborah <deborahcosta.f@hotmail.com>
2020-06-25 13:55:54 -0300

    ver tamanho de cada linha

--- aula4/main.cpp
+++ aula4/main.cpp
@@ -1,1 +1,29 @@
-#
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]){
+    std::ifstream file("messages2.txt");
+
+    if (!file.is_open()){
+        std::cerr <<"Arquivo não existe ou sem permissão de leitura"<<std::endl;
+        return 1;
+    }
+
+    std::string message;
+    int line_number =0;
+
+    while (!file.eof()){
+        ++line_number;
+        std::getline(file,message);
+        std::cout<< "tamanho da mensagem: "<< message.size()<<std::endl;
+        if (message.size()==0){
+            continue;
+        }
+        std::cout<< line_number<<". "<<message <<std::endl;
+    }
+
+    file.close();
+
+    return 0;
+}

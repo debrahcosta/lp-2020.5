600760dd22b8dc516a447d42b3a736a5fb2d2f66
déborah <deborahcosta.f@hotmail.com>
2020-06-25 10:25:46 -0300

    correção do professor

--- aula4/main.cpp
+++ aula4/main.cpp
@@ -5,7 +5,6 @@
 void show_usage(const std::string &prog_name);
 
 int main(int argc, char *argv[]){
-    std::ofstream arquivo_saida("arquivo.txt",std::ios::app);
 
     if(argc==1){
         show_usage(argv[0]);
@@ -29,8 +28,13 @@ int main(int argc, char *argv[]){
         message = argv[2];
     }
 
+    std::ofstream arquivo_saida("arquivo.txt",std::ios::app);
     arquivo_saida << message<<std::endl;
-
+    if(! arquivo_saida.is_open()){
+        std::cerr<<"O arquivo não pode ser criado" <<std::endl;
+        return 1;
+    }
+    arquivo_saida.close(); /*só preciso do close pra fechar em momento específico*/
     std::cout<<"Mensagem Adicionada"<<std::endl;
 
     return 0;

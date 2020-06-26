92c6407d8bd987e694b43a619adf67856ffef5a0
déborah <deborahcosta.f@hotmail.com>
2020-06-25 21:58:33 -0300

    versão para entregar

--- aula5/main.cpp
+++ aula5/main.cpp
@@ -19,6 +19,16 @@ std::string get_current_time() { return format_current_date("%H:%M:%S"); }
 
 void show_usage(const std::string &prog_name);
 
+void verifica_ocorrencia(std::ifstream &file, int ocorrencia);
+
+void list_diary(std::ifstream &file,std::string &message);
+
+int verifica_abertura(std::ifstream &file);
+
+int verifica_criacao(std::ofstream &file);
+
+void write_message(std::ofstream &arquivo_saida,int ocorrencia, std::string &message);
+
 int main(int argc, char *argv[]){
 
     if(argc==1){
@@ -40,17 +50,8 @@ int main(int argc, char *argv[]){
         std::getline(std::cin, message);
     }else if (argc==2 && action=="list"){
         std::ifstream file("arquivo.md");
-
-        if (!file.is_open()){
-            std::cerr <<"Arquivo não existe ou sem permissão de leitura"<<std::endl;
-            return 1;
-        }
-
-        while (!file.eof()){
-            std::getline(file,message);
-            std::cout<< message<<std::endl;
-        }
-
+        verifica_abertura(file);
+        list_diary(file,message);
         file.close();
         return 0;
 
@@ -61,7 +62,24 @@ int main(int argc, char *argv[]){
     std::ofstream arquivo_saida("arquivo.md",std::ios::app);
 
     std::ifstream file("arquivo.md",std::ios::app);
+
     int ocorrencia=0;
+
+    verifica_ocorrencia(file, ocorrencia);
+
+    write_message(arquivo_saida,ocorrencia, message);
+
+    arquivo_saida.close(); /*só preciso do close pra fechar em momento específico*/
+
+    std::cout<<"Mensagem Adicionada"<<std::endl;
+
+    return 0;
+}
+void show_usage(const std::string &prog_name) {
+    std::cout << "Uso: " << prog_name << " add <mensagem> ou "<<prog_name<<" list" << std::endl;
+}
+
+void verifica_ocorrencia(std::ifstream &file, int ocorrencia){
     while(!file.eof()){
         std::string line;
         std::getline(file,line);
@@ -70,21 +88,38 @@ int main(int argc, char *argv[]){
             ocorrencia++;
         }
     }
+}
 
+void list_diary(std::ifstream &file, std::string &message){
+    while (!file.eof()){
+        std::getline(file,message);
+        std::cout<< message<<std::endl;
+    }
+
+}
+
+int verifica_abertura(std::ifstream &file){
+    if (!file.is_open()){
+        std::cerr <<"Arquivo não existe ou sem permissão de leitura"<<std::endl;
+        return 1;
+    }
+    return 0;
+}
+
+int verifica_criacao(std::ofstream &file){
+    if (!file.is_open()){
+        std::cerr <<"O arquivo não pode ser criado"<<std::endl;
+        return 1;
+    }
+    return 0;
+}
+
+void write_message(std::ofstream &arquivo_saida, int ocorrencia, std::string &message){
     if(ocorrencia==0) {
         arquivo_saida << std::endl<<get_current_date() << std::endl << std::endl << "- " << get_current_time() << " " << message << std::endl;
-        if (!arquivo_saida.is_open()) {
-            std::cerr << "O arquivo não pode ser criado" << std::endl;
-            return 1;
-        }
+        verifica_criacao(arquivo_saida);
     } else{
         arquivo_saida << "- " << get_current_time() << " " << message<< std::endl;
     }
-    arquivo_saida.close(); /*só preciso do close pra fechar em momento específico*/
-    std::cout<<"Mensagem Adicionada"<<std::endl;
 
-    return 0;
-}
-void show_usage(const std::string &prog_name) {
-    std::cout << "Uso: " << prog_name << " add <mensagem> ou "<<prog_name<<" list" << std::endl;
 }
